60b101b61468cc9209fc7f66d3388f047cba68cb
Jacknot <jakom@x>
2019-02-28 17:28:47 +0100

    Javadoc a Registrazioni/Sessioni

--- src/EventBook/versione2/Registrazioni.java
+++ src/EventBook/versione2/Registrazioni.java
@@ -11,17 +11,34 @@ import java.util.ArrayList;
 import EventBook.versione2.fruitore.Fruitore;
 import EventBook.versione2.fruitore.Messaggio;
 
+/**Classe che contiene la lista di tutti i Fruitori registrati al programma.<br>
+ * @author Matteo Salvalai [715827], Lorenzo Maestrini[715780], Jacopo Mora [715149]
+ *
+ */
 public class Registrazioni implements Serializable{
 
 	private static final String NOMEFILE = "Registrazioni.ser";
 	
+	/**
+	 * Lista contenente i fruitori registrati
+	 */
 	private ArrayList<Fruitore> fruitori;
+	/**
+	 * Istanza necessaria per poter implementare il Design Pattern Singleton
+	 */
 	private static Registrazioni instance;
 	
+	/**
+	 * Costruttore
+	 */
 	private Registrazioni() {
 		fruitori = new ArrayList<Fruitore>();
 	}
 	
+	/**Metodo per ottenere l'istanza della classe<br>
+	 * Necessaria per il Design Pattern Singleton
+	 * @return l'istanza della classe
+	 */
 	public static Registrazioni getInstance() {
 		if(instance == null)
 			instance = new Registrazioni();
@@ -29,6 +46,11 @@ public class Registrazioni implements Serializable{
 	
 	}
 	
+	/**
+	 * Controlla se il fruitore è registrato
+	 * @param name Il nome del fruitore da cercare
+	 * @return True - se esiste un fruitore con tale nome <br>False - se il fruitore non esiste
+	 */
 	public boolean contains(String nome) {
 		for(Fruitore fruitore: fruitori) {
 			if(fruitore.getNome().equals(nome))
@@ -37,10 +59,19 @@ public class Registrazioni implements Serializable{
 		return false;
 	}
 	
+	/**
+	 * Registra un fruitore nella lista Registrazioni
+	 * @param nome Nome del fruitore da registrare
+	 */
 	public void registra(String nome) {
 		fruitori.add(new Fruitore(nome));
 	}
 	
+	/**
+	 * Permette al fruitore specificato di ricevere il Messaggio inviato come parametro
+	 * @param nome nome del fruitore
+	 * @param messaggio messaggio da inviare al fruitore
+	 */
 	public void ricevi(String nome, Messaggio messaggio) {
 		for(Fruitore fruitore: fruitori) {
 			if(fruitore.getNome().equals(nome))
@@ -48,6 +79,10 @@ public class Registrazioni implements Serializable{
 		}
 	}
 	
+	/**
+	 * Salva l'oggetto Registrazioni su un file per uso futuro
+	 * @throws IOException errore di I/O
+	 */
 	public void save() throws IOException {
 		FileOutputStream fileOut = new FileOutputStream(NOMEFILE);
 		ObjectOutputStream out = new ObjectOutputStream(fileOut);
@@ -56,6 +91,11 @@ public class Registrazioni implements Serializable{
 		fileOut.close();
 	}
 	
+	/**
+	 * Carica da file l'oggetto Registrazioni e lo assegna alla variabile instance
+	 * @throws ClassNotFoundException La classe dell'oggetto serializzato non può essere trovata
+	 * @throws IOException errore di I/O
+	 */
 	public void load() throws ClassNotFoundException, IOException {
 		FileInputStream fileIn = new FileInputStream(NOMEFILE);
         ObjectInputStream in = new ObjectInputStream(fileIn);
@@ -63,12 +103,5 @@ public class Registrazioni implements Serializable{
         in.close();
         fileIn.close();
 	}
-
-	public ArrayList<Fruitore> getFruitori() {
-		return fruitori;
-	}
-
-	public void setFruitori(ArrayList<Fruitore> fruitori) {
-		this.fruitori = fruitori;
-	}
+	
 }
--- src/EventBook/versione2/Sessione.java
+++ src/EventBook/versione2/Sessione.java
@@ -2,36 +2,60 @@ package EventBook.versione2;
 
 import java.util.ArrayList;
 
+/**Classe che consente di tenere in meomoria le proposte di un fruitore non ancora pubblicate, in modo da consentirne una modifica futura<br>
+ * @author Matteo Salvalai [715827], Lorenzo Maestrini[715780], Jacopo Mora [715149]
+ *
+ */
 public class Sessione {
-
+	/**
+	 * Il nome del fruitore proprietario della sessione
+	 */
 	private String proprietario;
+	
+	/**
+	 * Elenco di proposte che il proprietario ha in sospeso nella sessione corrente
+	 */
 	private ArrayList<Proposta> insiemeProposte;
 	
+	/**
+	 * Costruttore
+	 * @param _proprietario Il nome del fruitore proprietario della sessione
+	 */
 	public Sessione(String _proprietario) {
 		proprietario = _proprietario;
 		insiemeProposte = new ArrayList<Proposta>();
 	}
 
+	/**
+	 * Restituisce il proprietario della sessione
+	 * @return Nome del fruitore proprietario
+	 */
 	public String getProprietario() {
 		return proprietario;
 	}
 
-	public void setProprietario(String proprietario) {
-		this.proprietario = proprietario;
-	}
-
+	/**
+	 * Restituisce l'elenco delle proposte del fruitore
+	 * @return Elenco di proposte
+	 */
 	public ArrayList<Proposta> getInsiemeProposte() {
 		return insiemeProposte;
 	}
 
-	public void setInsiemeProposte(ArrayList<Proposta> insiemeProposte) {
-		this.insiemeProposte = insiemeProposte;
-	}
-	
+	/**
+	 * Aggiunge una proposta all'elenco delle proposte
+	 * @param proposta Proposta da aggiungere all'elenco
+	 */
 	public void aggiungiProposta(Proposta proposta) {
 		insiemeProposte.add(proposta);
 	}
 	
+	/**
+	 * Modifica una proposta esistente (?)
+	 * @param propostaSelezionata (?) String o direttamente oggetto?
+	 * @param nome (?)
+	 * @param valore Nuovo valore da sostituire
+	 */
 	public void modificaProposta(Proposta propostaSelezionata, String nome, String valore) {
 		for(Proposta proposta: insiemeProposte) {
 			if(proposta.equals(propostaSelezionata))
@@ -39,8 +63,12 @@ public class Sessione {
 		}
 	}
 	
+	/**
+	 * Pubblica la proposta in bacheca
+	 * @param proposta Proposta da pubblicare
+	 */
 	public void pubblica(Proposta proposta) {
-		//
+		//bacheca.aggiungi(proposta)
 	}
 	
 }
