5d752bf7d88669089f3add42669f36d45c32aa20
salvaEbasta <31668481+salvaEbasta@users.noreply.github.com>
2019-03-02 17:58:25 +0100

    introduzione Bacheca

---
+++ src/EventBook/versione2/Bacheca.java
@@ -0,0 +1,13 @@
+package EventBook.versione2;
+
+import EventBook.versione2.proposta.InsiemePropostePersistente;
+
+/**
+ * Ha il compito di tenere un set persistente di proposte visibili a tutti gli utenti<br>
+ * Implementa il Design Pattern Singleton
+ * @author Matteo
+ *
+ */
+public class Bacheca extends InsiemePropostePersistente{
+//Si può eventualmente imporre un interfaccia (GestoreProposte) se sono presenti operazioni pericolose
+}
---
+++ src/EventBook/versione2/proposta/InsiemePropostePersistente.java
@@ -0,0 +1,10 @@
+package EventBook.versione2.proposta;
+
+/**
+ * Un set di proposte in grado di mantenerle in modo persistente
+ * @author Matteo
+ *
+ */
+public class InsiemePropostePersistente {
+
+}
